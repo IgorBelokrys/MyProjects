33e6d3357d17c876a0857048abef934bd5a298e3
igorbel <igor.belokrys.96@bk.ru>
2025-01-12 19:38:31 +0300

    my proj

---
+++ treker/scripts/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
+/* utils */
+
+function loadData() {
+  const habbitsString = localStorage.getItem(HABBIT_KEY);
+  const habbitArray = JSON.parse(habbitsString);
+  if (Array.isArray(habbitArray)) {
+    habbits = habbitArray;
+  }
+}
+
+function saveData() {
+  localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+/* render */
+function rerenderMenu(activeHabbitId) {
+  if (!activeHabbitId) {
+    return;
+  }
+  for (const habbit of habbits) {
+  }
+}
+
+/* init */
+(() => {
+  loadData();
+})();
